b72e4a9293412e0cba4bcfa3a36bca4fd5eefc4b
RobertDevilee <302488043@student.rocmondriaan.nl>
2023-02-15 10:27:40 +0100

    homepage ingeladen

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,19 +2,29 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
     public function number(): Response
     {
         $number = random_int(0, 100);
-        return  $this->render('bezoker/number.html.twig', ['number'=>$number]);
+        return $this->render('bezoker/number.html.twig', ['number' => $number]);
 
 
-        return new Response(
-//            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+//        return new Response(
+////            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
     }
+        #[Route('/Home')]
+          public function HomePage(): Response
+    {
+        $date =date('L\t\h\e\ js');
+        return  $this->render('bezoker/home.html.twig', ['HomePage'=>$date]);
+
+    }
+
 }
